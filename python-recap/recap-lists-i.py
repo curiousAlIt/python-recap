2b2f4ff62fad420f81c1a6350fb3d077975863e9
curiousAlIt <email.amitadam@gmail.com>
2018-06-16 19:59:44 +0300

    recap of lists

---
+++ python-recap/recap-lists-i.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+a = [1, 3, 14, 344]
+print('hi we are coding in python')
+print('a')
+print(a)
+l = ['eggs','milk', 'cucumbers']
+print(l)
+
+#
+# length of the list
+#
+#
+
+print('the length of the list a:')
+print(len(a))
+
+#
+# adding an element to the list
+# 
+
+print('the list a before we add:')
+print(a)
+
+a.append(11235813)
+a.append(-7)
+a.append(22)
+print('the list a after we add:')
+print(a)
+
+# 
+# access to specific elements
+#
+
+print('The first item in a:')
+print(a[0])
+print('The second item in a:')
+print(a[1])
+print('The last item in a:')
+print(a[len(a)-1])
+
