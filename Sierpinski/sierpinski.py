8e6fbca2b617cc3fad55c17a97eda23a615e7781
curiousAlIt <email.amitadam@gmail.com>
2018-07-12 16:57:13 +0300

    much faster - one plot at the end

--- Sierpinski/sierpinski.py
+++ Sierpinski/sierpinski.py
@@ -18,8 +18,8 @@ def mid_point(a, b, c, d, r):
 
 
 
-anchors_x = [10, 70, 40]
-anchors_y = [10, 10, 40]
+anchors_x = [0, 100, 50]
+anchors_y = [0, 0, 70]
 
 plt.figure()
 plt.scatter(anchors_x, anchors_y, marker='o', color='r')
@@ -27,14 +27,22 @@ plt.scatter(anchors_x, anchors_y, marker='o', color='r')
 current_x = 0
 current_y = 0
 
-for k in range (0,4000):
+num_loops = 100000
+hx = [None] * num_loops
+hy = [None] * num_loops
+
+for k in range (0,num_loops):
 
     i = random.randint(0,len(anchors_x)-1)
     new_x, new_y = mid_point(current_x, current_y, anchors_x[i], anchors_y[i], 0.5)
 
-    plt.scatter(new_x, new_y, marker='.', color='k')
+    #plt.scatter(new_x, new_y, marker='.', color='k')
 
     current_x = new_x
     current_y = new_y
+    
+    hx[k] = current_x
+    hy[k] = current_y
 
+plt.scatter(hx, hy, marker = '.', color='k')
 
